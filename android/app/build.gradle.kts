71206cb725df3c8cd3cfc603eba7a4ca84790a55
NoCoder504W <nocode504w@gmail.com>
2025-11-23 18:18:34 +0100

    FIX : APK building

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.portefeuille"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
